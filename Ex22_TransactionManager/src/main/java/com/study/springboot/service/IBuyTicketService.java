81a39cf07deb0f7ccd5b45ee141c2f5b3ab3b1a0
Soondae9 <64182514+Soondae9@users.noreply.github.com>
2020-07-10 12:34:52 +0900

    a
    asd

--- Ex22_TransactionManager/src/main/java/com/study/springboot/service/IBuyTicketService.java
+++
@@ -1,6 +0,0 @@
-package com.study.springboot.service;
-
-public interface IBuyTicketService {
-	public int buy(String consumerId, int money, String error);
-
-}
--- Ex23_TransactionTemplate/src/main/java/com/study/springboot/dto/Transaction1Dto.java
+++
@@ -1,9 +0,0 @@
-package com.study.springboot.dto;
-
-import lombok.Data;
-
-@Data
-public class Transaction1Dto {
-	private String consumerId;
-	private int amount;
-}
