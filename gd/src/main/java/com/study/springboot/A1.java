5c2f349777f1584dfcb975fac62cc17f98c234b4
Soondae9 <64182514+Soondae9@users.noreply.github.com>
2020-07-10 13:39:42 +0900

    A

---
+++ gd/src/main/java/com/study/springboot/A1.java
@@ -0,0 +1,5 @@
+package com.study.springboot;
+
+public class A1 {
+
+}
---
+++ gd/src/main/java/com/study/springboot/GdApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdApplication.class, args);
+	}
+
+}
