0fc28c7fc422dfc68b8d39ef952cd8b359781055
Soondae9 <64182514+Soondae9@users.noreply.github.com>
2020-07-10 16:32:58 +0900

    asd

--- choice/src/main/java/com/example/demo/MainController.java
+++ choice/src/main/java/com/example/demo/MainController.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String root() throws Exception {
+		
+		return "main";
+	}
+	
 }
