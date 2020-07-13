d26b36f840c511678606ce975708e699cf5ba61f
user <user@i7C-57>
2020-07-13 16:38:03 +0900

    2020 07 13 16:37

--- choice/src/main/java/com/study/springboot/MainController.java
+++ choice/src/main/java/com/study/springboot/MainController.java
@@ -1,7 +1,6 @@
 package com.study.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,35 +9,80 @@ public class MainController {
 	@RequestMapping("/")
 	public String index() throws Exception {
 		
-		return "views/index";
+		return "index";
+	}
+	
+	@RequestMapping("/joinForm")
+	public String joinForm() throws Exception {
+		
+		return "member/joinForm";
 	}
 	
 	@RequestMapping("/loginForm")
 	public String loginForm() throws Exception {
 		
-		return "views/loginForm";
-	}
-	@RequestMapping("/list")
-	public String list() throws Exception {
-		
-		
-		
-		
-		return "bbs/list";
+		return "member/loginForm";
 	}
 	
 	
-	@RequestMapping("/joinForm")
-	public String joinForm() throws Exception {
+	@RequestMapping("/modifyForm")
+	public String modifyForm() throws Exception {
 		
-		return "views/joinForm";
+		return "member/modifyForm";
 	}
 	
-	@RequestMapping("/DeleteForm")
-	public String DeleteForm() throws Exception {
+	@RequestMapping("/cartList")
+	public String cartList() throws Exception {
 		
-		return "views/DeleteForm";
+		return "mypage/cartList";
 	}
+	
+	@RequestMapping("/coupon")
+	public String coupon() throws Exception {
+		
+		return "mypage/coupon";
+	}
+	
+	
+	
+	@RequestMapping("/deleteMember")
+	public String deleteMember() throws Exception {
+		
+		return "mypage/deleteMember";
+	}
+	
+	@RequestMapping("/modify")
+	public String modify() throws Exception {
+		
+		return "mypage/modify";
+	}
+	
+	@RequestMapping("/myPage")
+	public String myPage() throws Exception {
+		
+		return "mypage/myPage";
+	}
+	
+	@RequestMapping("/orderList")
+	public String orderList() throws Exception {
+	
+		return "mypage/orderList";
+	}
+	
+	@RequestMapping("/noticelist")
+	public String noticelist() throws Exception {
+	
+		return "notice/noticelist";
+	}
+	
+	@RequestMapping("/reviewlist")
+	public String reviewlist() throws Exception {
+	
+		return "review/reviewList";
+	}
+	
+	
+	
 	
 	
 	
